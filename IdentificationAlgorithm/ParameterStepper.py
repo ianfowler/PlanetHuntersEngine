d3134bf40b6f80f4a448dcfd107987039acc4f94
Ian Fowler <Ian@scn-111-125.airstudent.imsa.edu>
2020-02-05 09:01:42 -0600

    Bug fixes

--- IdentificationAlgorithm/ParameterStepper.py
+++ IdentificationAlgorithm/ParameterStepper.py
@@ -26,6 +26,7 @@ bins(100,100)
 #Inner orbital radius of habitable zone
 def roi(temp):
     return (0.62817*temp**3)-(1235.15*temp**2)
+
 #Outer orbital radius
 def roo(temp):
     return (1.52*temp**3)-(2988.75*temp**2)
@@ -45,18 +46,6 @@ def transitDepth(planetRadius,starRadius):
 def orbitalPeriod(randOrbital,starMass):
     return (2*math.pi*randOrbital**1.5)*math.sqrt((randOrbital*10^11)/(starMass*6.67))
 
-
-step_df = pd.DataFrame(columns=["count","t0" ,"per","rp","a" ,"inc","ecc","w"])
-count=0
-timeBetweenMeasure=20/(24*60)
-t0=0 #time of injunction
-orbitalInclination=0
-eccentricity=0
-
-
-steps_p = 50
-steps_o = 50
-
 def oradius_range(midTemp, steps=steps_o):
     roi_ =int(roi(midTemp))
     roo_ =int(roo(midTemp))
@@ -69,33 +58,37 @@ def pradius_range(midTemp, steps=steps_p):
     stepfinder_pradius = int((min_planet_pradius + max_planet_pradius)/50)
     return range(min_planet_pradius,max_planet_pradius,stepfinder_pradius)
 
-rows_list = []
-for bins in range (1,len(binTempArr)):
-    upper=binTempArr[bins]
-    lower=binTempArr[bins-1]
-    midTemp=(lower+upper)/2 # Approximation of temperature
+def gen_param_csv(filename, steps_p=50, steps_o=50, t0=0, orbitalInclination=0, eccentricity=0)
+    rows_list = []
+    for bins in range (1,len(binTempArr)):
+        upper=binTempArr[bins]
+        lower=binTempArr[bins-1]
+        midTemp=(lower+upper)/2 # Approximation of temperature
 
-    starRadius_ = starRadius(midTemp)
-    starMass_ = starMass(midTemp)
-    
-    for pradius in pradius_range(midTemp):#Planet radius; 50 steps
-        for oradius in oradius_range(midTemp):#Orbital radius; 50 steps -> 2500 steps per bin
-            # transitTime_ =(transitTime(starRadius_,oradius,starMass_))/60 #Minutes
-            orbitalPeriod_ =orbitalPeriod(oradius,starMass_)  #Find Units
+        starRadius_ = starRadius(midTemp)
+        starMass_ = starMass(midTemp)
+        
+        for pradius in pradius_range(midTemp):#Planet radius; 50 steps
+            for oradius in oradius_range(midTemp):#Orbital radius; 50 steps -> 2500 steps per bin
+                # transitTime_ =(transitTime(starRadius_,oradius,starMass_))/60 #Minutes
+                orbitalPeriod_ =orbitalPeriod(oradius,starMass_)  #Find Units
 
-            # params=np.array([count,t0,orbitalPeriod2,pradius,oradius,orbitalInclination,eccentricity,timeBetweenMeasure,transitTime_ ,total_measurements])
-            rows_list.append({
-                "bin_number":bins,
-                "lower_bin":lower,
-                "upper_bin":upper,
-                "temperature":midTemp,
-                "t0":t0,
-                "per":orbitalPeriod_,
-                "rp":pradius,
-                "a":oradius,
-                "inc":orbitalInclination,
-                "ecc":eccentricity,
-                "w":0,
-            })
+                # params=np.array([count,t0,orbitalPeriod2,pradius,oradius,orbitalInclination,eccentricity,timeBetweenMeasure,transitTime_ ,total_measurements])
+                rows_list.append({
+                    "bin_number":bins,
+                    "lower_bin":lower,
+                    "upper_bin":upper,
+                    "temperature":midTemp,
+                    "t0":t0,
+                    "per":orbitalPeriod_,
+                    "rp":pradius,
+                    "a":oradius,
+                    "inc":orbitalInclination,
+                    "ecc":eccentricity,
+                    "w":0,
+                })
 
-pd.DataFrame(rows_list).to_csv("bin_params.csv")
+    pd.DataFrame(rows_list).to_csv(filename)
+
+
+gen_param_csv("bin_params.csv")
