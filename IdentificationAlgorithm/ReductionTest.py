256b9868ef48dd05827e315a943f7734ec5806dd
Ian Son <ianson011@gmail.com>
2020-02-05 00:02:14 -0600

    made empy csvs, edited reduction test

---
+++ IdentificationAlgorithm/ReductionTest.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 29 18:35:20 2019
+
+@author: nageshsinghchauhan
+"""
+import os
+import warnings
+import math
+warnings.filterwarnings('ignore')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+from pylab import rcParams
+rcParams['figure.figsize'] = 10, 6
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split 
+from sklearn import linear_model
+from sklearn.metrics import recall_score, classification_report, precision_score, confusion_matrix, accuracy_score
+from sklearn.preprocessing import StandardScaler, normalize
+from scipy import ndimage
+import seaborn as sns
+from pylab import rcParams
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import precision_score, recall_score,roc_curve,auc, f1_score, roc_auc_score,confusion_matrix, accuracy_score, classification_report
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.svm import SVC
+import csv
+
+categ = {2: 1,1: 0}
+test_data = pd.read_csv('exoTest.csv').fillna(0)
+test_data.LABEL = [categ[item] for item in test_data.LABEL]
+
+def reduce_memory(df):
+    """ iterate through all the columns of a dataframe and modify the data type
+        to reduce memory usage.        
+    """
+    start_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+    
+    for col in df.columns:
+        col_type = df[col].dtype
+        
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+        else:
+            df[col] = df[col].astype('category')
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+    return df
+
+test_data = reduce_memory(test_data)
+
+#splitting the data
+x_test = test_data.drop(["LABEL"],axis=1)
+y_test = test_data["LABEL"]
+
+#Normalizing the data
+x_test = normalize(x_test)
+
+#Applying of gaussian filter
+x_test = ndimage.filters.gaussian_filter(x_test, sigma=10)
+
+#Feature scaling
+std_scaler = StandardScaler()
+x_test = std_scaler.fit_transform(x_test)
+
+#Dimentioanlity reduction
+from sklearn.decomposition import PCA
+pca = PCA() 
+x_test = pca.transform(x_test)
+
+#Output
+with open("ReducedData.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerows(x_test)
